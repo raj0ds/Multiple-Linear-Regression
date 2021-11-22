2c97f6bb2c5c9df6014afa4828016a81bdc52aea
RAJEEV ANAND <rajeevanand840@gmail.com>
2021-11-22 17:06:32 +0530

    Update Multiple_Linear_Regression.py

--- Multiple_Linear_Regression.py
+++ Multiple_Linear_Regression.py
@@ -1,11 +1,11 @@
-Objectives
-After completing this lab you will be able to:
+# Objectives
+# After completing this lab you will be able to:
 
-Use scikit-learn to implement Multiple Linear Regression
-Create a model, train it, test it and use the model
+# Use scikit-learn to implement Multiple Linear Regression
+# Create a model, train it, test it and use the model
 
 
-Importing Needed packages
+# Importing Needed packages
 import matplotlib.pyplot as plt
 import pandas as pd
 import pylab as pl
@@ -13,17 +13,17 @@ import numpy as np
 %matplotlib inline
 
 
-Downloading Data¶
-To download the data, we will use !wget to download it from IBM Object Storage.
-!wget -O FuelConsumption.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv
+# Downloading Data¶
+# To download the data, we will use !wget to download it from IBM Object Storage.
+# !wget -O FuelConsumption.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%202/data/FuelConsumptionCo2.csv
 
-Reading the data in
+# Reading the data in
 df = pd.read_csv("FuelConsumption.csv")
 
 # take a look at the dataset
 df.head()
 
-Let's select some features that we want to use for regression.
+# Let's select some features that we want to use for regression.
 cdf = df[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_CITY','FUELCONSUMPTION_HWY','FUELCONSUMPTION_COMB','CO2EMISSIONS']]
 cdf.head(9)
 
@@ -35,18 +35,18 @@ plt.ylabel("Emission")
 plt.show()
 
 
-Creating train and test dataset
-Train/Test Split involves splitting the dataset into training and testing sets respectively, which are mutually exclusive. After which, you train with the training set and test with the testing set. This will provide a more accurate evaluation on out-of-sample accuracy because the testing dataset is not part of the dataset that have been used to train the model. Therefore, it gives us a better understanding of how well our model generalizes on new data.
+# Creating train and test dataset
+# Train/Test Split involves splitting the dataset into training and testing sets respectively, which are mutually exclusive. After which, you train with the training set and test with the testing set. This will provide a more accurate evaluation on out-of-sample accuracy because the testing dataset is not part of the dataset that have been used to train the model. Therefore, it gives us a better understanding of how well our model generalizes on new data.
 
-We know the outcome of each data point in the testing dataset, making it great to test with! Since this data has not been used to train the model, the model has no knowledge of the outcome of these data points. So, in essence, it is truly an out-of-sample testing.
+# We know the outcome of each data point in the testing dataset, making it great to test with! Since this data has not been used to train the model, the model has no knowledge of the outcome of these data points. So, in essence, it is truly an out-of-sample testing.
 
-Let's split our dataset into train and test sets. Around 80% of the entire dataset will be used for training and 20% for testing. We create a mask to select random rows using the np.random.rand() function:
+# Let's split our dataset into train and test sets. Around 80% of the entire dataset will be used for training and 20% for testing. We create a mask to select random rows using the np.random.rand() function:
 
 msk = np.random.rand(len(df)) < 0.8
 train = cdf[msk]
 test = cdf[~msk]
 
-Train data distribution
+# Train data distribution
 
 plt.scatter(train.ENGINESIZE, train.CO2EMISSIONS,  color='blue')
 plt.xlabel("Engine size")
@@ -54,8 +54,8 @@ plt.ylabel("Emission")
 plt.show()
 
 
-Multiple Regression Model
-In reality, there are multiple variables that impact the co2emission. When more than one independent variable is present, the process is called multiple linear regression. An example of multiple linear regression is predicting co2emission using the features FUELCONSUMPTION_COMB, EngineSize and Cylinders of cars. The good thing here is that multiple linear regression model is the extension of the simple linear regression model.
+# Multiple Regression Model
+# In reality, there are multiple variables that impact the co2emission. When more than one independent variable is present, the process is called multiple linear regression. An example of multiple linear regression is predicting co2emission using the features FUELCONSUMPTION_COMB, EngineSize and Cylinders of cars. The good thing here is that multiple linear regression model is the extension of the simple linear regression model.
 
 from sklearn import linear_model
 regr = linear_model.LinearRegression()
@@ -65,17 +65,17 @@ regr.fit (x, y)
 # The coefficients
 print ('Coefficients: ', regr.coef_)
 
-As mentioned before, Coefficient and Intercept are the parameters of the fitted line. Given that it is a multiple linear regression model with 3 parameters and that the parameters are the intercept and coefficients of the hyperplane, sklearn can estimate them from our data. Scikit-learn uses plain Ordinary Least Squares method to solve this problem.
+# As mentioned before, Coefficient and Intercept are the parameters of the fitted line. Given that it is a multiple linear regression model with 3 parameters and that the parameters are the intercept and coefficients of the hyperplane, sklearn can estimate them from our data. Scikit-learn uses plain Ordinary Least Squares method to solve this problem.
 
-Ordinary Least Squares (OLS)¶
-OLS is a method for estimating the unknown parameters in a linear regression model. OLS chooses the parameters of a linear function of a set of explanatory variables by minimizing the sum of the squares of the differences between the target dependent variable and those predicted by the linear function. In other words, it tries to minimizes the sum of squared errors (SSE) or mean squared error (MSE) between the target variable (y) and our predicted output ( 𝑦̂  ) over all samples in the dataset.
+# Ordinary Least Squares (OLS)¶
+# OLS is a method for estimating the unknown parameters in a linear regression model. OLS chooses the parameters of a linear function of a set of explanatory variables by minimizing the sum of the squares of the differences between the target dependent variable and those predicted by the linear function. In other words, it tries to minimizes the sum of squared errors (SSE) or mean squared error (MSE) between the target variable (y) and our predicted output ( 𝑦̂  ) over all samples in the dataset.
 
-OLS can find the best parameters using of the following methods:
+# OLS can find the best parameters using of the following methods:
 
-Solving the model parameters analytically using closed-form equations
-Using an optimization algorithm (Gradient Descent, Stochastic Gradient Descent, Newton’s Method, etc.)
+# Solving the model parameters analytically using closed-form equations
+# Using an optimization algorithm (Gradient Descent, Stochastic Gradient Descent, Newton’s Method, etc.)
 
-Prediction
+# Prediction
 
 y_hat= regr.predict(test[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
 x = np.asanyarray(test[['ENGINESIZE','CYLINDERS','FUELCONSUMPTION_COMB']])
@@ -99,4 +99,4 @@ print("Residual sum of squares: %.2f"% np.mean((y_ - y) ** 2))
 print('Variance score: %.2f' % regr.score(x, y))
 
 
-Try to use a multiple linear regression with the same dataset, but this time use FUELCONSUMPTION_CITY and FUELCONSUMPTION_HWY instead of FUELCONSUMPTION_COMB. Does it result in better accuracy?
+# Try to use a multiple linear regression with the same dataset, but this time use FUELCONSUMPTION_CITY and FUELCONSUMPTION_HWY instead of FUELCONSUMPTION_COMB. Does it result in better accuracy?
